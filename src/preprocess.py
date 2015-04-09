7e206c720e39ef5187bc9282b601300f7d369eda
Matt Mulholland <mulhodm@gmail.com>
2015-04-09 01:13:22 -0400

    Update preprocess.py.
    Starting to piece together what this will actually look like.

--- src/preprocess.py
+++ src/preprocess.py
@@ -16,90 +16,105 @@ class Review(object):
     Class for objects representing Reviews.
     '''
 
-    def __init__(self, text, time, author, game):
+    # Attributes
+    orig = None # original text
+    hours_played = None # length of time played
+    game_id = None # game ID (corresponds to Steam's 'appid')
+    norm = None # str representing normalized review text
+    tok_sents = None # list of tokenized sentences (lists of str)
+    stem_sents = None # list of stemmed sentences (lists of str)
+    # Note: we could decide to lemmatize the text instead
+    pos_sents = None # list of POS-tagged sentences (lists of
+        # tuples containing tokens (str) and POS tags (str))
+    parsed_sents = None # list of parsed sentences
+    ngrams = Counter() # frequency distribution of token n-grams
+    lc_ngrams = Counter() # frequency distribution of lower-cased
+        # token n-grams
+    char_ngrams = Counter() # frequency distribution of character
+        # n-grams
+    lc_char_ngrams = Counter # frequency distribution of lower-cased
+        # character n-grams
+    stem_ngrams = Counter() # frequency distribution of stemmed
+        # token n-grams
+    dep = None # frequency distribution of syntactic dependencies
+    #suffix_tree = None # suffix tree representing text
+
+    def __init__(self, review_text, hours_played, game_id):
         '''
         Initialization method.
 
-        :param text: review text
-        :type text: str
-        :param time: amount of time author spent playing game (in hours)
-        :type time: int
-        :param author: author name
-        :type author: str
-        :param game: game name
-        :type game: str
+        :param review_text: review text
+        :type review_text: str
+        :param hours_played: length of time author spent playing game
+        :type hours_played: int
+        :param game_id: game ID
+        :type game_id: str
         '''
 
-        # Make sure time is an int (or can be interpreted as an int, at least)
+        # Make sure time is a float (or can be interpreted as an int, at least)
         try:
-            time = int(time)
+            hours_played = float(hours_played)
         except ValueError:
-            sys.exit('ERROR: The \"time\" parameter that was passed in, {},'
-                     'could not be typecast as an int.\n\n'
-                     'Exiting.\n'.format(time))
+            sys.exit('ERROR: The \"hours_played\" parameter that was passed in, {},'
+                     'could not be typecast as a float.\n\n'
+                     'Exiting.\n'.format(hours_played))
 
-        # Initialization attributes
-        self.orig_text = text
-        self.time = time
-        self.author = author
-        self.game = game
-
-        # Text-related attributes
-        self.norm_text = None # str representing normalize text
-        self.tok_text = None # list of tokenized sentences (lists of str)
-        self.stem_text = None # list of stemmed sentences (lists of str)
-        # Note: we could decide to lemmatize the text instead
-        self.pos_text = None # list of POS-tagged sentences (lists of tuples
-            # containing tokens (str) and POS tags (str))
-        self.dep_text = None # list of syntactic parse trees
-        self.tok_fdist = Counter() # frequency distribution of tokens
-        self.stem_fdist = Counter() # frequency distribution of stems
-        self.suffix_tree = None # suffix tree representing text
+        self.orig = review_text
+        self.hours_played = hours_played
+        self.game_id = game_id
 
         # Generate attribute values
-        self.normalize_text()
-        self.tokenize_text()
-        self.stem_text()
-        self.pos_tag_text()
-        self.parse_text()
-        self.generate_fdist()
-        self.generate_suffix_tree()
+        self.norm = normalize()
+        self.tok_sents = tokenize()
+        self.stem_sents = stem()
+        self.pos_sents = pos_tag()
+        self.parsed_sents = parse()
+        #self.dep = 
+        #self.ngrams = 
+        #self.lc_ngrams = 
+        #self.char_ngrams = 
+        #self.lc_char_ngrams = 
+        #self.stem_ngrams = 
+        #self.generate_suffix_tree()
 
-
-    def normalize_text(self):
+    @staticmethod
+    def normalize():
         '''
         Perform text preprocessing, i.e., lower-casing, etc., to generate the norm_text attribute.
         '''
 
         # Collapse all sequences of one or more whitespace characters, strip
         # whitespace off the ends of the string, and lower-case all characters
-        self.norm_text = re.sub(r'[\n\t ]+',
-                                r' ',
-                                self.orig_text.strip().lower())
+        return re.sub(r'[\n\t ]+',
+                      r' ',
+                      self.orig.strip().lower())
 
 
-    def tokenize_text(self):
+    @staticmethod
+    def tokenize():
         '''
         Perform tokenization using NLTK's sentence/word tokenizers.
         '''
 
-        sents = sent_tokenize(self.norm_text)
-        self.tok_text = [word_tokenize(sent) for sent in sents]
+        sents = sent_tokenize(self.norm)
+        return [word_tokenize(sent) for sent in sents]
 
 
-    def stem_text(self):
+    @staticmethod
+    def stem():
         '''
         Perform stemming
         '''
 
-        stemmer = SnowballStemmer("english")
+        sb = SnowballStemmer("english")
         stemmed_sents = []
-        for sent in self.tok_text:
-            stemmed_sents.append([stemmer.stem(tok) for tok in sent])
-        self.stem_text = stemmed_sents
+        for sent in self.tok_sents:
+            stemmed_sents.append([sb.stem(tok) for tok in sent])
+        return stemmed_sents
 
 
-    def pos_tag_text(self):
+    @staticmethod
+    def pos_tag():
         '''
         Run NLTK POS-tagger on text.
         '''
@@ -107,7 +122,8 @@ class Review(object):
         raise NotImplementedError
 
 
-    def parse_text(self):
+    @staticmethod
+    def parse():
         '''
         Parse sentences.
         '''
@@ -115,20 +131,21 @@ class Review(object):
         raise NotImplementedError
 
 
-    def generate_fdist(self):
+    @staticmethod
+    def generate_ngram_fdist(sents, min=1, max=5):
         '''
         Generate frequency distribution for the tokens in the text (and possibly also for the lemmas or stems).
+
+        :param sents: list of sentence-corresponding lists (of characters, tokens, etc.) that can be chopped up into n-grams.
+        :type sents: list of lists/strs
+        :param min: minimum value of n for n-gram extraction
+        :type min: int
+        :param max: maximum value of n for n-gram extraction
+        :type max: int
+        :returns: Counter
         '''
 
-        [self.tok_fdist.update(sent) for sent in self.tok_text]
-
-
-    def generate_stem_fdist(self):
-        '''
-        Generate frequency distribution for the tokens in the text (and possibly also for the lemmas or stems).
-        '''
-
-        [self.stem_fdist.update(sent) for sent in self.stem_text]
+        raise NotImplementedError
 
 
     def generate_suffix_tree(self, max_depth=5):
