f0ec117278871c79995a02d3b9ba71f351dbee1b
Matt Mulholland <mulhodm@gmail.com>
2015-04-14 03:38:42 -0400

    Added a couple new scripts that I have been working on.

---
+++ data/__init__.py
@@ -0,0 +1,10 @@
+APPID_DICT = dict(Dota_2='570',
+                  Counter_Strike_Global_Offensive='730',
+                  Team_Fortress_2='440',
+                  Football_Manager_2015='295270',
+                  Sid_Meiers_Civilization_5='8930',
+                  Garrys_Mod='4000',
+                  Arma_3='107410',
+                  The_Elder_Scrolls_V='72850',
+                  Warframe='230410',
+                  Counter_Strike='10')
