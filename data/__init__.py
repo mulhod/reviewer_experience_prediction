cd1030af0ec57d6cb53c3ac3c4821e55f8930f8b
Emily Olshefski <emilygraceolshefski@gmail.com>
2015-04-22 18:06:46 -0400

    added GTAV
    maybe delete (already have one iteration from the Counter Strike series

--- data/__init__.py
+++ data/__init__.py
@@ -1,10 +1,12 @@
 APPID_DICT = dict(Dota_2='570',
                   Counter_Strike_Global_Offensive='730',
                   Team_Fortress_2='440',
+                   Grand_Theft_Auto_V='271590',
                   Football_Manager_2015='295270',
                   Sid_Meiers_Civilization_5='8930',
                   Garrys_Mod='4000',
                   Arma_3='107410',
                   The_Elder_Scrolls_V='72850',
                   Warframe='230410',
-                  Counter_Strike='10')
+                  Counter_Strike='10'
+                 )
