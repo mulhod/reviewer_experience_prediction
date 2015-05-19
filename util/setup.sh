65b9a2ad53b3fc675a1728b1112065b750dd63d4
Matt Mulholland <mulhodm@gmail.com>
2015-05-19 01:27:06 -0400

    Modified the setup.sh script, but it still probably doesn't work.

--- util/setup.sh
+++ util/setup.sh
@@ -2,10 +2,12 @@
 
 # Usage: ./setup.sh
 
+set -eu
+
 cd $(dirname $(dirname $0))
 conda config --add channels pypi
 conda config --add channels dan_blanchard
 conda create --yes -n reviews python=3.4
 source activate reviews
 conda install --yes --file conda_requirements.txt || (echo "Wouldn't fucking work. Exiting."; exit 1)
-pip install langdetect argparse
+pip install skll langdetect argparse
