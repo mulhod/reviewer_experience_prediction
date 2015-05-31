fd2b5b1e2c9481b218e51c3889ad1665f16ab711
mulhod <mulhodm@gmail.com>
2015-05-31 01:22:27 -0400

    Get rid of local-only script for starting MongoDB server

--- util/start_mongodb_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-
-# Starts MongoDB server using a particular database store
-# To be used on Montclair's lemur server only
-
-mongod --dbpath /home/mulhollandm2/usr/local/share/mongodb/data/db
