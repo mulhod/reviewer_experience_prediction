13cde5817dafa180de1b2846d618e816f63edcf6
Matt Mulholland <mulhodm@gmail.com>
2015-06-07 01:17:45 -0400

    Complete a working setup.py and integrate usage of it into setup.sh

--- util/setup.py
+++ util/setup.py
@@ -1,31 +1,42 @@
-from distutils.core import setup
-from subprocess import getoutput
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
+from os import listdir
+from sys import exit
+from shutil import copy
 from os.path import (dirname,
                      realpath,
                      join)
+from subprocess import getoutput
+from distutils.core import setup
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+from distutils.extension import Extension
 
 util_dir = dirname(realpath(__file__))
 src_dir = join(dirname(util_dir),
                'src')
+build_dir = join(dirname(util_dir),
+                 'build')
+
+# Hackish way of doing this. Find better way...
 root_env = getoutput("conda info | grep \"root environment :\""
                      " | awk '{print $4}'")
 python_header_dir = join(root_env,
                          'pkgs/python-3.4.3-0/include/python3.4m')
 
+ext_names = {'feat_ext': 'feature_extraction',
+             'data': 'datasets',
+             'mongo': 'mongodb'}
+
 ext_modules = [Extension('feature_extraction',
                          [join(src_dir,
-                               "feature_extraction.pyx")],
+                               "{0}.pyx".format(ext_names['feat_ext']))],
                          include_dirs=[python_header_dir]),
                Extension('datasets',
                          [join(util_dir,
-                               "datasets.pyx")],
+                               "{0}.pyx".format(ext_names['data']))],
                          include_dirs=[python_header_dir]),
                Extension('mongodb',
                          [join(util_dir,
-                               "mongodb.pyx")],
+                               "{0}.pyx".format(ext_names['mongo']))],
                          include_dirs=[python_header_dir])]
 
 setup(
@@ -33,3 +44,25 @@ setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = cythonize(ext_modules)
 )
+
+# Copy files from build directory
+build_libs_dir = [join(build_dir,
+                       _dir) for _dir in listdir(build_dir) if
+                  _dir.startswith('lib')]
+if not build_libs_dir or len(build_libs_dir) > 1:
+    exit('Could not find lib* directory in build/. Exiting.\n')
+build_libs_dir = build_libs_dir[0]
+exts = listdir(build_libs_dir)
+for ext in exts:
+    if ext.startswith('feature_extraction'):
+        copy(join(build_libs_dir,
+                  ext),
+             join(src_dir,
+                  '{}.so'.format(ext.split('.',
+                                           1)[0])))
+    else:
+        copy(join(build_libs_dir,
+                  ext),
+             join(util_dir,
+                  '{}.so'.format(ext.split('.',
+                                           1)[0])))
--- util/setup.sh
+++ util/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-# Not really in a working state right now (?)
+# Works on some types of systems, but maybe not others
+# Note: having conda installed is a requirement (currently)
 
 # Usage: ./setup.sh
 
@@ -11,24 +12,30 @@ echo "Make sure that conda (miniconda) is installed before trying to set up" \
 
 echo "Creating conda environment...\n"
 conda config --add channels pypi
+# Create environment first and force python3.4 (for some reason, just adding
+# python=3.4 to the list of packages in conda_requirements.txt does not work
+# as it is not recognized as a valid package name (wtf...).
 conda create --yes -n reviews python=3.4
+# And now install all of the packages we need
 source activate reviews
 conda install --yes --file conda_requirements.txt || \
-    (echo "Wouldn't fucking work. Exiting."; exit 1)
+    (echo "Wouldn't fucking work. Exiting.\n"; exit 1)
 echo "Created \"reviews\" environment successfully! To use environment, run" \
      "\"source activate reviews\". To get out of the environment, run" \
-     "\"source deactivate\"."
+     "\"source deactivate\".\n"
 
 echo "Installing some extra packages with pip (since conda does not seem to" \
-     "want to install them)..."
+     "want to install them)...\n"
 pip install skll langdetect argparse
 
 # Download model data for spaCy
 echo "Downloading model data for spaCy package...\n"
 python3.4 -m spacy.en.download
 
-# Cythonize .pyx modules..."
+# Cythonize .pyx modules and compile
+echo "Cythonizing and compiling *.pyx modules...\n"
 UTIL_DIR=$(dirname $(readlink -f $0))
-${UTIL_DIR}/cythonize.sh
+python3.4 ${UTIL_DIR}/setup.py build_ext
 
-echo "Complete."
+echo "Setup complete. Use \"source activate reviews\" to use conda"
+     "environment.\n"
