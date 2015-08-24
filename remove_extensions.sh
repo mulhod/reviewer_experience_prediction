2b453d9b56f5e5058bebbe961c481cf3dfbf443e
Matthew Mulholland <mulhodm@gmail.com>
2015-08-24 02:05:02 -0400

    Fix bugs in remove_extensions.sh utility

--- remove_extensions.sh
+++ remove_extensions.sh
@@ -4,6 +4,39 @@
 
 # Usage: ./remove_extensions.sh
 
-THIS_DIR=$(readlink -f $0)
-rm -fR ${THIS_DIR}/build ${THIS_DIR}/src/*.c ${THIS_DIR}/src/*.so \
-    ${THIS_DIR}/util/*so util/*.c ${THIS_DIR}/*.so &
+THIS_DIR=$(dirname $(readlink -f $0))
+if [[ -e ${THIS_DIR}/build ]]; then
+    
+    rm -fR ${THIS_DIR}/build
+    
+fi
+if [[ $(ls ${THIS_DIR}/src | grep -P "\.c$" | wc -l) -gt 0 ]]; then
+    
+    rm -f ${THIS_DIR}/src/*.c
+    
+fi
+if [[ $(ls ${THIS_DIR}/src | grep -P "\.so$" | wc -l) -gt 0 ]]; then
+
+    rm -f ${THIS_DIR}/src/*.so
+
+fi
+if [[ $(ls ${THIS_DIR}/util | grep -P "\.c$" | wc -l) -gt 0 ]]; then
+
+    rm -f ${THIS_DIR}/util/*.c
+
+fi
+if [[ $(ls ${THIS_DIR}/util | grep -P "\.so$" | wc -l) -gt 0 ]]; then
+
+    rm -f ${THIS_DIR}/util/*.so
+
+fi
+if [[ $(ls ${THIS_DIR} | grep -P "\.c$" | wc -l) -gt 0 ]]; then
+
+    rm -f ${THIS_DIR}/*.c
+
+fi
+if [[ $(ls ${THIS_DIR} | grep -P "\.so$" | wc -l) -gt 0 ]]; then
+
+    rm -f ${THIS_DIR}/*.so
+
+fi
