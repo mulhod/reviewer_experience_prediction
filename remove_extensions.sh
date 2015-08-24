6f64df31ab3658a43e2967e63ef34583678abeed
Matt Mulholland <mulhodm@gmail.com>
2015-08-24 01:07:32 -0400

    Fix problem in remove_extensions.sh utility

--- remove_extensions.sh
+++ remove_extensions.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
-# Removes the build directory and all files associated with extensions (except the extension modules themselves!).
-# Also removes *pyc and __pycache__ files/directories.
+# Removes the build directory and all files associated with extensions (except
+# the extension modules themselves!).
 
 # Usage: ./remove_extensions.sh
 
-rm -fR build src/*c src/*so src/*pyc src/__pycache__ util/*so util/*c util/*pyc util/__pycache__ &
+rm -fR build src/*c src/*so util/*so util/*c &
