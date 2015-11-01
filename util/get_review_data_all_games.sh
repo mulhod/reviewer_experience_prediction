858cd727b764c98b40d77067f1cb1441ca86b202
Matthew Mulholland <mmulholland@ets.org>
2015-11-01 15:06:57 -0500

    Update review data collection script

--- util/get_review_data_all_games.sh
+++ util/get_review_data_all_games.sh
@@ -14,47 +14,22 @@ else
     
 fi
 
-python3.4 util/get_review_data.py \
-    --games Warframe \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Warframe.txt &
-python3.4 util/get_review_data.py \
-    --games The_Elder_Scrolls_V \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_The_Elder_Scrolls_V.txt &
-python3.4 util/get_review_data.py \
-    --games Team_Fortress_2 \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Team_Fortress_2.txt &
-python3.4 util/get_review_data.py \
-    --games Sid_Meiers_Civilization_5 \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Sid_Meiers_Civilization_5.txt &
-python3.4 util/get_review_data.py \
-    --games Grand_Theft_Auto_V \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Grand_Theft_Auto_V.txt &
-python3.4 util/get_review_data.py \
-    --games Garrys_Mod \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Garrys_Mod.txt &
-python3.4 util/get_review_data.py \
-    --games Football_Manager_2015 \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Football_Manager_2015.txt &
-python3.4 util/get_review_data.py \
-    --games Arma_3 \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Arma_3.txt &
-python3.4 util/get_review_data.py \
-    --games Dota_2 \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Dota_2.txt &
-python3.4 util/get_review_data.py \
-    --games Counter_Strike_Global_Offensive \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Counter_Strike_Global_Offensive.txt &
-python3.4 util/get_review_data.py \
-    --games Counter_Strike \
-    --wait ${WAIT_TIME} \
-    -log logs/replog_get_review_data_Counter_Strike.txt &
+DATA_DIR="$(dirname $(dirname $(readlink -f $0)))/data"
+GAME_FILES=( $(ls ${DATA_DIR}/*jsonlines \
+               | grep -v "sample" \
+               | awk -F/ '{print $NF}' ) )
+wd=`pwd`
+cd "${DATA_DIR}/.."
+for game in ${GAME_FILES}; do
+    
+    echo "Getting review data for ${game}..."
+    python3.4 util/get_review_data.py \
+        --games ${game} \
+        --wait ${WAIT_TIME} \
+        -log logs/replog_get_review_data_${game}.txt &
+    sleep 5s
+    
+done
+
+cd "${wd}"
+echo "Complete.\n"
