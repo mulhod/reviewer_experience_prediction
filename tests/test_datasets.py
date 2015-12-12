ad9296f4cdc18e9089a31ec13ce44c028b53ee09
Matthew Mulholland <mulhodm@gmail.com>
2015-12-12 04:50:33 -0500

    Fix tests for the get_bin and validate_bin_ranges functions

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -9,13 +9,13 @@ def test_get_bin():
 
     vals = [0.1, 0.0001, 1.5, 1.99, 2.5]
     expected_bins = [1, -1, 1, 1, 2]
-    bin_ranges = [(0.0, 2.0), (2.1, 3.0)]
+    bin_ranges = [(0.1, 2.0), (2.1, 3.0)]
 
     for i, val in enumerate(vals):
-        assert_equal(val, expected_bins[i])
+        assert_equal(get_bin(bin_ranges, val), expected_bins[i])
 
 
-def test_validate_bins_ranges_invalid():
+def test_validate_bin_ranges_invalid():
     """
     Test the `validate_bin_ranges` function given invalid `bin_ranges`
     values.
@@ -24,14 +24,14 @@ def test_validate_bins_ranges_invalid():
     bin_ranges_invalid = [[(0.0, 19.9)], [(0.0, 1.1), (2.1, 2.9)],
                           [(5.1, 2.1), (2.0, 1.3)]]
     for bin_ranges in bin_ranges_invalid:
-        assert_equal(get_bin(bin_ranges), False)
+        assert_equal(validate_bin_ranges(bin_ranges), False)
 
 
-def test_validate_bins_ranges_invalid():
+def test_validate_bin_ranges_invalid():
     """
     Test the `validate_bin_ranges` function given valid `bin_ranges`
     values.
     """
 
     bin_ranges_valid = [(0.0, 90.1), (90.2, 104.5), (104.6, 150.9)]
-    assert_equal(get_bin(bin_ranges_valid, True))
+    assert_equal(validate_bin_ranges(bin_ranges_valid), True)
