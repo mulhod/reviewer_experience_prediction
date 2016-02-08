c640bc0f4cb4e46435f63aaae6b5870641108756
Matthew Mulholland <mulhodm@gmail.com>
2016-02-08 11:17:32 -0500

    Try to fix a bug in setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ THIS_DIR=$(dirname $(readlink -f $0))
 cd ${THIS_DIR}
 
 echo "Make sure that conda (miniconda) is installed before trying to set up" \
-     "or else this script will fail...\n"
+     "or else this script will fail..."
 
-echo "Creating conda environment...\n"
+echo "Creating conda environment..."
 conda config --add channels pypi
 # Create environment first and force python=3.4 (for some reason, just
 # adding python=3.4 to the list of packages in conda_requirements.txt
@@ -26,36 +26,35 @@ source activate reviews
 conda install --yes --file conda_requirements.txt
 if [ $? -gt 0 ]; then
     echo "\"conda install --yes --file conda_requirements.txt\" failed. " \
-         "Exiting.\n"
+         "Exiting."
     cd ${ORIG_DIR}
     exit 1
 fi
 echo "Created \"reviews\" environment successfully! To use environment, run" \
      "\"source activate reviews\". To get out of the environment, run" \
-     "\"source deactivate\".\n"
+     "\"source deactivate\"."
 
 echo "Installing some extra packages with pip (since conda does not seem to" \
-     "want to install them)...\n"
+     "want to install them)..."
 pip install skll==1.1.0 langdetect==1.0.5 argparse pudb nose2==0.5.0 typing==3.5.0.1 schema==0.4.0
 if [ $? -gt 0 ]; then
-    echo "pip installation of langdetect and argparse failed. Exiting.\n"
+    echo "pip installation of langdetect and argparse failed. Exiting."
     cd ${ORIG_DIR}
     exit 1
 fi
 
 # Download model data for spaCy
-echo "Downloading model data for spaCy package...\n"
+echo "Downloading model data for spaCy package..."
 python3.4 -m spacy.en.download
 
 # Compile Cython modules
-echo "Compiling Cython extensions...\n"
+echo "Compiling Cython extensions..."
 python3.4 setup.py install
 echo "Package installed!"
 echo "If changes are made to the Cython extensions, run the following to " \
      "re-compile the extensions for use in the various command-line " \
-     "utilities:\n\tpython setup.py build_ext\n\nOr run the following to " \
-     "reinstall the entire package: python setup.py install\n"
+     "utilities: \"python setup.py build_ext\". Or run the following to " \
+     "reinstall the entire package: \"python setup.py install\""
 
 echo "Setup complete. Use \"source activate reviews\" to activate conda" \
-     "environment.\n"
-cd ${ORIG_DIR}
+     "environment."
